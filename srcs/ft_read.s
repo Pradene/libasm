a8e02e2e12dc965758135993b9094e02825f60f3
Pradene <liam.pradene@gmail.com>
2025-05-22 21:25:11 +0200

    Set errno when error happens in read/write

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -1,7 +1,26 @@
 section .text
-  global ft_write
+  global ft_read
+  extern __errno_location
 
-ft_write:
+ft_read:
+  ; call read
   mov rax, 0
   syscall
+
+  ; test if return of read < 0
+  test rax, rax
+  js .error
+
+  ret
+
+.error:
+  ; set errno = -rax
+  neg rax
+  mov rdi, rax
+
+  call __errno_location ; get pointer to errno
+  mov [rax], edi ; store errno inside int
+
+  ; return -1
+  mov rax, -1
   ret
--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,7 +1,26 @@
 section .text
   global ft_write
+  extern __errno_location
 
 ft_write:
+  ; call write
   mov rax, 1
   syscall
+
+  ; test if return of write < 0
+  test rax, rax
+  js .error
+
+  ret
+
+.error:
+  ; set errno = -rax
+  neg rax
+  mov rdi, rax
+
+  call __errno_location ; get pointer to errno
+  mov [rax], edi ; store errno inside int
+
+  ; return -1
+  mov rax, -1
   ret
