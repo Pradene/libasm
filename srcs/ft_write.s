8ff73eb9d1c13d62901b681dfc7cd1b98ce2486a
Pradene <liam.pradene@gmail.com>
2025-06-04 14:53:46 +0200

    Handle NULL in ft_write

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -3,24 +3,31 @@ section .text
   extern __errno_location
 
 ft_write:
+  ; Check if buffer is NULL
+  test rsi, rsi
+  jz .null_error
+  
   ; call write
   mov rax, 1
   syscall
-
   ; test if return of write < 0
   test rax, rax
   js .error
+  ret
 
+.null_error:
+  ; set errno = EINVAL (22)
+  call __errno_location wrt ..plt
+  mov dword [rax], 22  ; EINVAL = 22
+  mov rax, -1
   ret
 
 .error:
   ; set errno = -rax
   neg rax
   mov rdi, rax
-
-  call __errno_location wrt ..plt ; get pointer to errno
-  mov [rax], edi ; store errno inside int
-
+  call __errno_location wrt ..plt
+  mov [rax], edi
   ; return -1
   mov rax, -1
   ret
